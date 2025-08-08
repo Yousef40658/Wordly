310ad9c2dc9819926f1af71a3aaa743ec045c038
Yousef40658 <youssefmohamed0585@gmail.com>
2025-08-08 17:01:36 +0300

    added fetching the selected word by reading a text file -> more words and flexability to customize

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import random
 import requests
+import os
 
 
 class Worlde :
     def __init__(self, word_length = 5 , max_attempts = 6):
         self.word_length = word_length 
         self.max_attempts = max_attempts
-        selected_word = self.select_word(word_length)
+        self.selected_word = self.select_word(word_length)
 
 
 
     def select_word(self,word_length) :                               #passed as a parameter to reduce dependency 
-        #Dataset
-        pattern = "?" * word_length                                   #n? returns words with n letters
-        url = f"https://api.datamuse.com/words?sp={pattern}&max=1000" #maximum 1000 words , #builds url
-        response = requests.get(url)                                  #request to the API and getting response 
-        words = response.json()                                        #parses the response into a dictonery
+        file_name = f'{word_length}-letter-words.txt'                 #3-letter-words.txt , 4-letter-words.txt
+        folder_path = os.path.dirname(os.path.abspath(__file__))      
+        file_path = os.path.join(folder_path,file_name)               #full_path -> no need to run from the folder place
+        
+        #reading file
+        with open(file_path) as f :                                   #storing all words in memory to fetch from it instead of file -> faster
+            self.words = f.read().splitlines()                        #splits by \n default , self. to be used by other methods
+        self.n_of_words = len(self.words)                             #much more words than API -> even bigger datasets can be used
 
-        n_of_words = len(words) #for_safety
-        selected_word_index = random.randint(0,n_of_words)
-        selected_word = words[selected_word_index]["word"]
-        print(selected_word)
-        return selected_word
+        #picking a random word
+        selected_word_index = random.randint(0,self.n_of_words)
+        return self.words[selected_word_index]
+
+
 
     
 
 wordle = Worlde()
+print(wordle.selected_word)
